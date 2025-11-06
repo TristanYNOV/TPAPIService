0fa019fe7ff5885142466aa02332f2e304dde560
TristanEPSI <tristan.gottschak@laposte.net>
2025-11-06 11:16:27 +0100

    fix compil error

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    // AuthModule,
+    // PostsModule,
+  ],
+  providers: [PrismaService],
 })
 export class AppModule {}
