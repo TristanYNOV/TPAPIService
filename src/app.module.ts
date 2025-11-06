e3a7d5ae3199907e8912d58029311c0438e8b44a
TristanEPSI <tristan.gottschak@laposte.net>
2025-11-06 16:51:41 +0100

    try fix 4

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 import { UsersModule } from './users/users.module';
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,          // charge .env au démarrage (globalement)
+      envFilePath: '.env',     // racine du projet
+    }),
     // Les variables d'environnement sont chargées via `dotenv/config` dans main.ts
     AuthModule,
     PostsModule,
